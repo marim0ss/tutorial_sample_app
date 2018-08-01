a9c3debacbd9460457fc0f60c9527fe7eeb9484a
marim0ss <piyomaru.bunbun@gmail.com>
2018-08-01 22:27:08 +0900

    validates 文字数,email形式,unique,大文字小文字は区別しない

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+
+  # before_save  保存直前に実行されるメソッド
+    #これで、データベースに保存される直前にemailの文字列は全て小文字変換される
+  before_save { self.email = email.downcase }
+
     validates :name,  presence: true, length: {maximum: 50 }
-    validates :email, presence: true, length: {maximum: 255 }
+
+    #正規表現で有効なemailを指定する このパターンに一致するemailだけが有効になる
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      validates :email, presence: true, length: { maximum: 255 },
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
+                              # case_sensitiveにfalseを指定すると、大文字小文字を差を無視する
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,19 +3,27 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 # 有効なUserかどうかをテストする
+  # setupメソッド内で、まず有効なUserオブジェクト(@user)を作成する
+  #setup内の処理は、各テストが走る直前に実行される
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
+# ここで@userが有効かどうかをテストしている
+  # assertメソッド：@userがtrueを返すと成功し、falseを返すと失敗する
   test "should be valid" do
     assert @user.valid?
   end
 
+# name属性があるかどうかのテスト
+  # @user.name=""  まず@user変数のname属性にから文字をセット
+  # assert_notメソッドで@userが有効ではなくなったことを確認している
   test "name should be present" do
     @user.name = ""
     assert_not @user.valid?
   end
 
+# emailについてもあるかどうかのテスト
   test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
@@ -23,12 +31,37 @@ class UserTest < ActiveSupport::TestCase
 
 # 文字数制限のテスト
   test "name should not be too long" do
-     @user.name = "a" * 51
-     assert_not @user.valid?
+     @user.name = "a" * 51       #51文字の文字列を作るために掛け算を書いてみる
+     assert_not @user.valid?     #これは有効でないことを確認
    end
 
    test "email should not be too long" do
-     @user.email = "a" * 244 + "@example.com"
-     assert_not @user.valid?
+     @user.email = "a" * 244 + "@example.com" #これも同じくa*244でemailが255文字オーバーするようにしてみる
+     assert_not @user.valid?     #これは有効でないことを確認
    end
+
+#有効なアドレス、無効なアドレス
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+    # ↑assertメソッドの第二引数にエラーメッセージを追加
+      # これによって、どのアドレスで失敗したかを特定できるようにしている
+      #inspectメソッド：p ~ と同じ。文字列を返す
+
+  # 重複するアドレスは拒否するテスト
+    #dup : 同じ属性を持つデータを複製するメソッド
+    # .saveした後になのでdupで複製したduplicate_userは無効のはず
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+
+    # 通常大文字・小文字は区別されないので、アドレスを大文字でも同じものとする
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
